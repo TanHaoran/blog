32b1c18ceab4f519748ed1c94f97998bb6846822
JerryTan <thrforever@126.com>
2018-07-08 20:14:43 +0800

    5-14 Thymeleaf实战-导入IDE，配置项目

---
+++ src/main/java/com/jerry/blog/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.jerry.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello world!";
+    }
+}
---
+++ src/test/java/com/jerry/blog/controller/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.jerry.blog.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHello() throws Exception {
+        mockMvc.perform(get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
